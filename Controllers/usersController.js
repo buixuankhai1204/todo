21b85be9fa753639f72d2e5bee2314527b3c1f81
buixuankhai1204 <khaibuixuan2@gmail.com>
2023-10-07 14:08:02 +0700

    insert

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -4,13 +4,19 @@ module.exports = class userController {
     static async getAllUser(req, res, next) {
 
 
-        const users = await  user.getAllUser();
-
-        return res.status(200).json({
-            status: "success",
-            data: users,
-            message: "get all users success"
-        })
+        const [users, status] = await user.getAllUser();
+        if (status === true) {
+            return res.status(200).json({
+                status: "success",
+                data: users,
+                message: "get all users success"
+            })
+        } else {
+            return res.status(401).json({
+                status: "fail",
+                message: users
+            })
+        }
     }
 
     static async insertUser(req, res, next) {
@@ -19,16 +25,24 @@ module.exports = class userController {
         const email = req.body.email;
         const age = req.body.age;
         const userRequest = {
-            name : name,
-            status : status,
-            email : email,
-            age : age,
+            name: name,
+            status: status,
+            email: email,
+            age: age,
         }
-        const userService = await user.insertUser(userRequest, next);
-        return res.status(200).json({
-            status: "success",
-            data: userService,
-            message: "insert user success"
-        })
+        const [userService, statusCheck] = await user.insertUser(userRequest, next);
+
+        if (statusCheck === false) {
+            return res.status(200).json({
+                status: "fail",
+                message: userService
+            })
+        } else {
+            return res.status(200).json({
+                status: "success",
+                data: userService,
+                message: "insert user success"
+            })
+        }
     }
 }
--- Services/user.js
+++ Services/user.js
@@ -1,43 +1,32 @@
 const db = require('../db');
 module.exports = class User {
     static async getAllUser(req, res, next) {
-
-        const [users, fields] = await db.execute('SELECT * FROM `user`');
-        console.log(users);
-        if(users.length ===0) {
-            return new Error('cannot get all user');
+        try {
+            const [users, fields] = await db.execute('SELECT * FROM `user`');
+            console.log(users);
+            if (users.length !== 0) {
+                return [users, true];
+            }
+        } catch (err) {
+            return [err.sqlMessage, false]
         }
 
-        return users;
     }
 
     static async insertUser(data, next) {
         try {
-            console.log(data);
             const [user] = await db.query('INSERT INTO `user` SET ?', data);
-                const [insertedRow] = await db.query('SELECT * FROM `user` WHERE id = ?', [user.insertId]);
-            if(insertedRow.length === 0) {
-                return new Error('can not insert new insertedRow');
+            const [insertedRow] = await db.query('SELECT * FROM `user` WHERE id = ?', [user.insertId]);
+            if (insertedRow.length !== 0) {
+                return [insertedRow, true];
             }
 
-            return insertedRow;
         } catch (err) {
-            console.log(err.sqlMessage)
+            return [err.sqlMessage, false]
         }
-
     }
 
     static async updateStatusUser(data, next) {
-        db.query('UPDATE INTO user SET ?', {
-            name: data.name,
-            status: data.status,
-            email: data.semail
-        }, function (err, users) {
-            if (err) {
-                return err;
-            } else {
-                return user;
-            }
-        })
+
     }
 }
